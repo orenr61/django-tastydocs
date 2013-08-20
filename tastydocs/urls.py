7f4ad60e830e2948776b79ccfb9b4ceef026bd36
Oren Rasekh <orenr61@gmail.com>
2013-08-20 16:49:49 -0400

    Added versions page. Now Capturing api_name in urls

--- tastydocs/urls.py
+++ tastydocs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns
 from views import doc
 
-urlpatterns = patterns(
-    '',
-    (r'^api/$', doc),
-    (r'^example/(?P<resource_name>.+)/', 'tastydocs.views.example_data'),
-)
+ urlpatterns = patterns(                                                                            
+     '',                                                                                            
+     (r'^api/$', versions),                                                                         
+     (r'^api/(?P<api_name>.+)$', doc),                                                              
+     (r'^example/(?P<api_name>.+)/(?P<resource_name>.+)/', 'tastydocs.views.example_data'),         
+ )                                                                                                  
